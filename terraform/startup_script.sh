29a4d3745cc779212d133be54dd894cd49474522
romanaumov <roman.naumov@gmail.com>
2025-04-19 04:09:44 +1200

    Modify terraform VM

--- terraform/startup_script.sh
+++ terraform/startup_script.sh
@@ -49,6 +49,10 @@ python3 load_gcp_secrets.py "trademe-viewer" "ENV_VAR_TRADEME" "$REPO_DIR/.env"
 python3 load_gcp_secrets.py "trademe-viewer" "CRED_VAR_TRADEME" "$REPO_DIR/airflow/keys/credentials.json"
 python3 load_gcp_secrets.py "trademe-viewer" "SERV_VAR_TRADEME" "$REPO_DIR/airflow/keys/trademe-viewer-service-account.json"
 
+sudo usermod -aG docker trademe
+sudo systemctl enable docker
+sudo systemctl start docker
+
 # Build and run the containers
 docker-compose build --no-cache
 docker-compose up -d
